c83155d9ce0d7b238369298b2520c4df0f131869
alone19bin <144338107+alone19bin@users.noreply.github.com>
2023-10-19 14:30:18 +0300

    Update TaskTwo.java

--- src/main/java/com/maksim/collections/TaskTwo.java
+++ src/main/java/com/maksim/collections/TaskTwo.java
@@ -8,13 +8,13 @@ import java.util.stream.IntStream;
 public class TaskTwo {
     public static void main(String[] args) {
         int[] nums = {4, 5, 6, 6, 8};
-        boolean res = Arrays.stream(nums)   //Стрим из массива
-                .boxed() //преобращование автобоксинг в integer для листа
-                .distinct()   //.возвращает новый поток, содержащий только уникальные элементы исходного потока
-                .toList()   //Этот метод возвращает коллектор, который накапливает входные элементы в новый List
-                .size() != nums.length; //возвращает размер списка, он не равен
+        boolean res = Arrays.stream(nums)  
+                .boxed() 
+                .distinct()   
+                .toList()  
+                .size() != nums.length; 
         System.out.println( res);
-    }///Просто сравните длину исходного массива с длиной четко отфильтрованного списка.
+    }
 }
 
 
