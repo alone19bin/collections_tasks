3e92711d8b941c09b9b9b24b0e8828ac6ec45e1f
alone19bin <144338107+alone19bin@users.noreply.github.com>
2023-10-19 14:28:48 +0300

    Update TaskOne.java

--- src/main/java/com/maksim/collections/TaskOne.java
+++ src/main/java/com/maksim/collections/TaskOne.java
@@ -6,19 +6,17 @@ import java.util.stream.IntStream;
 public class TaskOne {
     public static void main(String[] args) {
         int[] nums = {3, 8, 15, 17};
-        int target = 11; //
-        findPair(nums, target);
+        int sum = 11; //
+        search(nums, sum);
     }
-
-    public static void findPair(int[] nums, int sum) {
-        // рассмотреть каждый элемент, кроме последнего элемента
+/*TODO  -В первом цикле проходиммя по кажому элементу кроме посленего
+*         -Во втором с i до последнего элемента
+*           -Если сумма найдена, выводим ее   */
+    public static void search(int[] nums, int sum) {
         for (int i = 0; i < nums.length - 1; i++) {
-            // начать с i-го элемента до последнего элемента
             for (int j = i + 1; j < nums.length; j++) {
-                //если искомая сумма найдена, выведите ее и верните
                 if (nums[i] + nums[j] == sum) {
                     System.out.println("Индексы: " + i + " и " + j);
-                    return;
                 }
             }
         }
